6d7e72e00fbcbd8c7b8670ef85d065d437956ff1
monkey <fanghuawfh@gmail.com>
2024-03-03 13:04:09 +0800

    feat: 修改名字

--- src/wdyr.js
+++ src/wdyr.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
-  // if (__DEV__) {
-  console.log('wfh----');
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     trackAllPureComponents: true,
